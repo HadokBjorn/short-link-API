f5f6b375c30ea8df6d5f8a5dd4233da0245d1e57
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-17 21:52:51 -0500

    fix: removed insert confirmPassword

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,12 +1,13 @@
 import { db } from "../database/database.connections.js";
 
 export async function signup(req, res) {
-	const { name, email, password, confirmPassword } = req.body;
+	const { name, email, password } = req.body;
 	try {
-		await db.query(
-			`INSERT INTO users (name, email, password, "confirmPassword") VALUES ($1,$2,$3,$4)`,
-			[name, email, password, confirmPassword]
-		);
+		await db.query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3)`, [
+			name,
+			email,
+			password,
+		]);
 		res.sendStatus(201);
 	} catch (err) {
 		if (err.code === "23505") return res.sendStatus(409);
