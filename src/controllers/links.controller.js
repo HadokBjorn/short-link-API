816c8ff6b94cb840716820b0727ac12bf0abb355
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-19 20:56:07 -0500

    feat: open a url with redirect

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -35,3 +35,19 @@ export async function getUrlById(req, res) {
 		res.status(500).send(err.message);
 	}
 }
+
+export async function openUrl(req, res) {
+	const { shortUrl } = req.params;
+	try {
+		const response = await db.query(`SELECT id, url, visits FROM links WHERE "shortUrl"=$1;`, [
+			shortUrl,
+		]);
+		if (response.rowCount === 0) return res.sendStatus(404);
+		const { id, url, visits } = response.rows[0];
+		let visit = visits;
+		await db.query(`UPDATE links SET visits=$1 WHERE id=$2`, [(visit += 1), id]);
+		res.redirect(url);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+}
--- src/routers/links.router.js
+++ src/routers/links.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createShortUrl, getUrlById } from "../controllers/links.controller.js";
+import { createShortUrl, getUrlById, openUrl } from "../controllers/links.controller.js";
 import { authorization } from "../middlewares/auth.middleware.js";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/links.schema.js";
@@ -8,5 +8,6 @@ const linksRouter = Router();
 
 linksRouter.post("/urls/shorten", validateSchema(urlSchema), authorization, createShortUrl);
 linksRouter.get("/urls/:id", getUrlById);
+linksRouter.get("/urls/open/:shortUrl", openUrl);
 
 export default linksRouter;
