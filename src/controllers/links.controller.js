5165bc30487f85653a890e42a7013ee7b9ba5207
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-19 19:29:30 -0500

    feat: Get short url by id

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -2,7 +2,7 @@ import { db } from "../database/database.connections.js";
 import { nanoid } from "nanoid";
 
 export async function createShortUrl(req, res) {
-	const { id, name } = res.locals.user;
+	const { id } = res.locals.user;
 	const { url } = req.body;
 	const shortUrl = nanoid(10);
 	try {
@@ -17,6 +17,20 @@ export async function createShortUrl(req, res) {
 		]);
 
 		res.status(201).send(response.rows[0]);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+}
+
+export async function getUrlById(req, res) {
+	const { id } = req.params;
+	try {
+		const response = await db.query(`SELECT id, "shortUrl", url FROM links WHERE id=$1;`, [
+			Number(id),
+		]);
+		if (response.rowCount === 0) return res.sendStatus(404);
+
+		res.status(200).send(response.rows[0]);
 	} catch (err) {
 		res.status(500).send(err.message);
 	}
--- src/routers/links.router.js
+++ src/routers/links.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createShortUrl } from "../controllers/links.controller.js";
+import { createShortUrl, getUrlById } from "../controllers/links.controller.js";
 import { authorization } from "../middlewares/auth.middleware.js";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/links.schema.js";
@@ -7,5 +7,6 @@ import { urlSchema } from "../schemas/links.schema.js";
 const linksRouter = Router();
 
 linksRouter.post("/urls/shorten", validateSchema(urlSchema), authorization, createShortUrl);
+linksRouter.get("/urls/:id", getUrlById);
 
 export default linksRouter;
